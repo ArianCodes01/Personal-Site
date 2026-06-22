e962fe6720433573545d7e2453740c180b637f17
Arian Mortazavi <ariyanmortazavi56@gmail.com>
2026-06-22 10:08:02 -0700

    responsive-and-mistakes

---
+++ app.js
@@ -0,0 +1,10 @@
+let sidebar = document.querySelector('.sidebar')
+let barsIcon = document.getElementById('barsIcon')
+let closeBtn = document.getElementById('closeBtn')
+
+barsIcon.onclick = function(){
+    sidebar.style.right = 0
+}
+closeBtn.onclick = function(){
+    sidebar.style.right = '-280px'
+}
